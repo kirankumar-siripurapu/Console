9755bd26fbb1afa3da73bb73eb4db74f80c5107f
Kiran Kumar Siripurapu <siripurapu.kirancse@gmail.com>
2020-06-25 01:31:02 +0530

    commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! How are you ?");
+            Console.WriteLine("Hello World! How are you boss?");
         }
     }
 }
